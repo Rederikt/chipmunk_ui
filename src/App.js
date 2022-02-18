21c064134b3c3793c7c638c63f440d8856635eac
Rederikt <toxanes1999@gmail.com>
2022-02-19 00:32:41 +0200

    App created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 const App = () => {
-  <div>
-    <h1>Hello React</h1>
-  </div>
-}
+  return(
+    <div>
+      <h1>Chipmunk UI</h1>
+    </div>
+  )
+};
 
 export default App;
